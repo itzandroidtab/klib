163d084b6ab79c9c04a46a76047b29de120a1e4f
itzandroidtab <koen@koon.io>
2022-03-27 22:30:19 +0200

    added includes for swo and rtt streams

---
+++ klib/stream.hpp
@@ -0,0 +1,8 @@
+#ifndef KLIB_STREAM_HPP
+#define KLIB_STREAM_HPP
+
+// include the stream specific implementations
+#include <klib/comm/streams/rtt_stream.hpp>
+#include <klib/comm/streams/swo_stream.hpp>
+
+#endif
