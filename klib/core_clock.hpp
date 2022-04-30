f97e4534081f43d4e1ec91f660248755f7ed0092
itzandroidtab <koen@koon.io>
2022-04-30 23:01:27 +0200

    added core_clock for storing the cpu frequency

---
+++ klib/core_clock.hpp
@@ -0,0 +1,32 @@
+#ifndef KLIB_CLOCK_HPP
+#define KLIB_CLOCK_HPP
+
+#include <cstdint>
+
+namespace klib {
+    class clock {
+    protected:
+        // clock frequency of the cpu
+        static inline uint32_t core_clock;
+
+    public:
+        /**
+         * @brief Get the core clock speed of the cpu
+         * 
+         */
+        static uint32_t get() {
+            return core_clock;
+        }
+
+        /**
+         * @brief Change the core clock
+         * 
+         * @param clock 
+         */
+        static void set(const uint32_t clock) {
+            core_clock = clock;
+        }
+    };
+}
+
+#endif
