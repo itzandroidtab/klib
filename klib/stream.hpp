7d434b6f46afd09846e5e705d722763f9b9e3f02
itzandroidtab <koen@koon.io>
2022-05-28 11:11:16 +0200

    added default cout/cin to cmakelists

--- klib/stream.hpp
+++ klib/stream.hpp
@@ -5,4 +5,15 @@
 #include <klib/comm/streams/rtt_stream.hpp>
 #include <klib/comm/streams/swo_stream.hpp>
 
+namespace klib {
+    #if defined(KLIB_DEFAULT_COUT)
+        // create a cout to print data to the host machine
+        constexpr klib::KLIB_DEFAULT_COUT::cout cout;
+    #endif
+    #if defined(KLIB_DEFAULT_CIN)
+        // create a cin to print data to the host machine
+        constexpr klib::KLIB_DEFAULT_CIN::cin cin;
+    #endif
+}
+
 #endif
