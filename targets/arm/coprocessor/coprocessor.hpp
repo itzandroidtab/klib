54c1c0e3face607ee04cd76b5ce5eed9ae09c9f2
itzandroidtab <koendegraaff@itzandroidtab.nl>
2022-07-10 13:35:31 +0200

    added disable to clear the cpacr register before setting it

--- targets/arm/coprocessor/coprocessor.hpp
+++ targets/arm/coprocessor/coprocessor.hpp
@@ -27,6 +27,10 @@ namespace klib::arm {
         static void enable(const access right, volatile uint32_t *const cpacr) {
             static_assert(CoProcessorId < 14, "Max amount of coprocessors is 13");
 
+            // disable the coprocessors first to clear the previous value
+            disable<coprocessor>(cpacr);
+
+            // set the access right in the register
             (*cpacr) |= static_cast<uint8_t>(right) << (CoProcessorId * 2);
         }
 
