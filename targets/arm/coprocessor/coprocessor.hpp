1c597eed2186bbd74f8046f875754965fc476366
itzandroidtab <koendegraaff@itzandroidtab.nl>
2022-07-10 17:41:53 +0200

    changed enable to set and removed disable function

--- targets/arm/coprocessor/coprocessor.hpp
+++ targets/arm/coprocessor/coprocessor.hpp
@@ -24,28 +24,14 @@ namespace klib::arm {
          * @param cpacr pointer to the cpacr register
          */
         template <uint32_t CoProcessorId>
-        static void enable(const access right, volatile uint32_t *const cpacr) {
+        static void set(const access right, volatile uint32_t *const cpacr) {
             static_assert(CoProcessorId < 14, "Max amount of coprocessors is 13");
 
-            // disable the coprocessors first to clear the previous value
-            disable<coprocessor>(cpacr);
+            // clear the coprocessors first to clear the previous value
+            (*cpacr) &= ~(0x3 << (CoProcessorId * 2));
 
             // set the access right in the register
             (*cpacr) |= static_cast<uint8_t>(right) << (CoProcessorId * 2);
-        }
-
-        /**
-         * @brief Disable a coprocessor
-         * 
-         * @tparam CoProcessorId 
-         * @param right access right for the coprocessor
-         * @param cpacr pointer to the cpacr register
-         */
-        template <uint32_t CoProcessorId>
-        static void disable(const access right, volatile uint32_t *const cpacr) {
-            static_assert(CoProcessorId < 14, "Max amount of coprocessors is 13");
-
-            (*cpacr) &= ~(static_cast<uint8_t>(right) << (CoProcessorId * 2));
         }
     };
 }
