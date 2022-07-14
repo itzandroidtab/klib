5bb2c15d7b54816e0772a8f7656dcf6049d49d9b
itzandroidtab <koendegraaff@itzandroidtab.nl>
2022-07-14 20:44:34 +0200

    moved usb speed and version to setup

--- klib/usb/setup.hpp
+++ klib/usb/setup.hpp
@@ -57,6 +57,27 @@ namespace klib::usb {
 
 namespace klib::usb::setup {
     /**
+     * Speed mode of a USB device
+     * 
+     */
+    enum class usb_speed {
+        low_speed,
+        full_speed,
+        high_speed
+    };
+
+    /**
+     * Version of the USB device
+     * 
+     */
+    enum class usb_version {
+        usb_v1_0 = 0x0100,
+        usb_v1_1 = 0x0110,
+        usb_v2_0 = 0x0200,
+        usb_v2_1 = 0x0201
+    };
+
+    /**
      * USB request data transfer direction
      * (bmRequestType)
      * 
