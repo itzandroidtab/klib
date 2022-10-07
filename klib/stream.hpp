465d07984d9be0400ff2d2a75b6926f3d7047710
itzandroidtab <koen@koon.io>
2022-10-07 22:26:35 +0200

    added guards to prevent including rtt when rtt is not in source tree

--- klib/stream.hpp
+++ klib/stream.hpp
@@ -2,15 +2,23 @@
 #define KLIB_STREAM_HPP
 
 // include the stream specific implementations
-#include <klib/comm/streams/rtt_stream.hpp>
 #include <klib/comm/streams/swo_stream.hpp>
 
+// prevent the segger stream from being included
+// if we do not have segger support enabled
+#if (KLIB_SEGGER_SUPPORT == true)
+    #include <klib/comm/streams/rtt_stream.hpp>
+#endif
+
 namespace klib {
-    #if defined(KLIB_DEFAULT_COUT)
-        // create a cout to print data to the host machine
+    // check if we have a default stream we need to add.
+    // make sure that when RTT is setup we check if we 
+    // have segger support enabled.
+    #if defined(KLIB_DEFAULT_COUT) && !(KLIB_DEFAULT_COUT == rtt && KLIB_SEGGER_SUPPORT == false)
+        // create a cout to print data to the host machine	
         constexpr klib::KLIB_DEFAULT_COUT::cout cout;
     #endif
-    #if defined(KLIB_DEFAULT_CIN)
+    #if defined(KLIB_DEFAULT_CIN) && !(KLIB_DEFAULT_COUT == rtt && KLIB_SEGGER_SUPPORT == false)
         // create a cin to print data to the host machine
         constexpr klib::KLIB_DEFAULT_CIN::cin cin;
     #endif
