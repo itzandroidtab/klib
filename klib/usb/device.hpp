20fcbdcae095ee9f4df7f1ed79b82ad6af4e9cd2
itzandroidtab <koen@itzandroidtab.nl>
2022-11-30 00:17:03 +0100

    added device helper functions

--- klib/usb/device.hpp
+++ klib/usb/device.hpp
@@ -1,7 +1,32 @@
 #ifndef KLIB_USB_DEVICE_HPP
 #define KLIB_USB_DEVICE_HPP
 
-namespace klib::usb {
+#include <klib/usb/usb.hpp>
+
+namespace klib::usb::device {
+    /**
+     * @brief Helper function to convert a descriptor struct to a descriptor description
+     * 
+     * @tparam T 
+     * @param desc 
+     * @param size 
+     * @return constexpr description 
+     */
+    template <typename T>
+    constexpr usb::description descriptor_to_desc(const T& desc, const uint32_t size) {
+        return {reinterpret_cast<const uint8_t *const>(&desc), size};
+    }
+
+    /**
+     * @brief Helper function to convert a raw descriptor array to a descriptor description
+     * 
+     * @param desc 
+     * @param size 
+     * @return constexpr usb::description 
+     */
+    constexpr usb::description descriptor_to_desc(const uint8_t *const desc, const uint32_t size) {
+        return {desc, size};
+    }
 }
 
 #endif
