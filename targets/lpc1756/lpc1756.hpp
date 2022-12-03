679f7e5d876669d8408337f31df7eabd53739837
itzandroidtab <koen@itzandroidtab.nl>
2022-12-03 02:19:58 +0100

    added lpc1756 header

---
+++ targets/lpc1756/lpc1756.hpp
@@ -0,0 +1,39 @@
+#ifndef KLIB_LPC1756_HPP
+#define KLIB_LPC1756_HPP
+
+#include <klib/irq.hpp>
+
+// #include "lpc176x5x.h"
+
+namespace klib::lpc1756 {
+    // // irq for the lpc1756
+    // using irq = klib::irq<32>;
+
+    // /**
+    //  * @brief Enable a interrupt
+    //  * 
+    //  * @tparam Irq 
+    //  */
+    // template <uint32_t Irq>
+    // static void enable_irq() {
+    //     static_assert(Irq >= static_cast<uint32_t>(irq::arm_vector::count), "Invalid IRQ given to enable");
+
+    //     // enable the irq
+    //     NVIC_EnableIRQ(static_cast<IRQn_Type>(Irq - static_cast<uint32_t>(irq::arm_vector::count)));
+    // }
+
+    // /**
+    //  * @brief Disable a interrupt
+    //  * 
+    //  * @tparam Irq 
+    //  */
+    // template <uint32_t Irq>
+    // static void disable_irq() {
+    //     static_assert(Irq >= static_cast<uint32_t>(irq::arm_vector::count), "Invalid IRQ given to disable");
+
+    //     // disable the irq
+    //     NVIC_DisableIRQ(static_cast<IRQn_Type>(Irq - static_cast<uint32_t>(irq::arm_vector::count)));
+    // }
+}
+
+#endif
