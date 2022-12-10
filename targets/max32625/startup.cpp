2b60e99dba8b950eee0f79a1c09da77b030e1d49
itzandroidtab <koen@itzandroidtab.nl>
2022-12-10 22:33:37 +0100

    updated the max32625 startup

--- targets/max32625/startup.cpp
+++ targets/max32625/startup.cpp
@@ -1,7 +1,10 @@
 #include <cstdint>
 
+#include <klib/systick.hpp>
 #include <klib/core_clock.hpp>
 #include <coprocessor/coprocessor.hpp>
+
+#include <max32625.hpp>
 
 // disable the constructor does not take arguments error in vscode
 #ifdef __INTELLISENSE__
@@ -9,6 +12,8 @@
 #endif
 
 void __attribute__((__constructor__(101))) __target_startup() {
+    using target = klib::max32625;
+
     // change the core clock to the correct frequency
     klib::clock::set(96'000'000);
 
@@ -17,7 +22,16 @@ void __attribute__((__constructor__(101))) __target_startup() {
         using coprocessor = klib::arm::coprocessor;
 
         // enable the floating point coprocessors
-        // coprocessor::enable<10>(coprocessor::access::full, &SCB->CPACR);
-        // coprocessor::enable<11>(coprocessor::access::full, &SCB->CPACR);
+        coprocessor::set<10>(coprocessor::access::full, &SCB->CPACR);
+        coprocessor::set<11>(coprocessor::access::full, &SCB->CPACR);
     }
+
+    // init the systick timer
+    klib::systick::init<target::irq>();
+
+    // enable the systick timer
+    klib::systick::enable();
+
+    // enable MPU, bus and usage faults in seperate interrupts
+    // SCB->SHCSR = 0b1011;
 }
