faf11e257a9182be3ea1af62db5eecedfc23f83f
itzandroidtab <koen@itzandroidtab.nl>
2023-01-03 23:10:30 +0100

    commented target generated files in include for the lpc1788

--- targets/lpc1788/lpc1788.hpp
+++ targets/lpc1788/lpc1788.hpp
@@ -3,7 +3,7 @@
 
 #include <klib/irq.hpp>
 
-#include "lpc178x7x.h"
+// #include "lpc178x7x.h"
 
 namespace klib::lpc1788 {
     // irq for the lpc1788
@@ -16,10 +16,10 @@ namespace klib::lpc1788 {
      */
     template <uint32_t Irq>
     static void enable_irq() {
-        static_assert(Irq >= static_cast<uint32_t>(irq::arm_vector::count), "Invalid IRQ given to enable");
+        // static_assert(Irq >= static_cast<uint32_t>(irq::arm_vector::count), "Invalid IRQ given to enable");
 
-        // enable the irq
-        NVIC_EnableIRQ(static_cast<IRQn_Type>(Irq - static_cast<uint32_t>(irq::arm_vector::count)));
+        // // enable the irq
+        // NVIC_EnableIRQ(static_cast<IRQn_Type>(Irq - static_cast<uint32_t>(irq::arm_vector::count)));
     }
 
     /**
@@ -29,10 +29,10 @@ namespace klib::lpc1788 {
      */
     template <uint32_t Irq>
     static void disable_irq() {
-        static_assert(Irq >= static_cast<uint32_t>(irq::arm_vector::count), "Invalid IRQ given to disable");
+        // static_assert(Irq >= static_cast<uint32_t>(irq::arm_vector::count), "Invalid IRQ given to disable");
 
-        // disable the irq
-        NVIC_DisableIRQ(static_cast<IRQn_Type>(Irq - static_cast<uint32_t>(irq::arm_vector::count)));
+        // // disable the irq
+        // NVIC_DisableIRQ(static_cast<IRQn_Type>(Irq - static_cast<uint32_t>(irq::arm_vector::count)));
     }
 }
 
