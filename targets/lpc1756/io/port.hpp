ecbbd20234e6599b39242fe14ebb8dc1f760764b
itzandroidtab <koen@itzandroidtab.nl>
2023-03-05 23:15:15 +0100

    bugfix set peripheral not setting correct bits

--- targets/lpc1756/io/port.hpp
+++ targets/lpc1756/io/port.hpp
@@ -85,19 +85,19 @@ namespace klib::lpc1756::io::detail::pins {
         // set the 3 function registers
         if constexpr (std::is_same_v<Periph, io::detail::alternate::func_1>) {
             // setup alternate function 1
-            (*pin_select) = ((*pin_select) & ~(0b11 << (Pin::number / 2))) | (0b01 << (Pin::number / 2));;
+            (*pin_select) = ((*pin_select) & ~(0b11 << ((Pin::number * 2)) % 32)) | (0b01 << ((Pin::number * 2)) % 32);;
         }
         else if constexpr (std::is_same_v<Periph, io::detail::alternate::func_2>) {
             // setup alternate function 2
-            (*pin_select) = ((*pin_select) & ~(0b11 << (Pin::number / 2))) | (0b10 << (Pin::number / 2));
+            (*pin_select) = ((*pin_select) & ~(0b11 << ((Pin::number * 2)) % 32)) | (0b10 << ((Pin::number * 2)) % 32);
         }
         else if constexpr (std::is_same_v<Periph, io::detail::alternate::func_3>) {
             // setup alternate function 3
-            (*pin_select) |= (0b11 << (Pin::number / 2));
+            (*pin_select) |= (0b11 << ((Pin::number * 2)) % 32);
         }
         else {
             // setup normal gpio function
-            (*pin_select) &= ~(0b11 << (Pin::number / 2));
+            (*pin_select) &= ~(0b11 << ((Pin::number * 2)) % 32);
         }
     }
 }
