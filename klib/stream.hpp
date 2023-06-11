fdbe102f40e1facab03016ba7fa3ae403c09ce0c
itzandroidtab <koen@itzandroidtab.nl>
2023-06-11 16:09:11 +0200

    bugfix rtt cout case. Is already handled in the rtt driver

--- klib/stream.hpp
+++ klib/stream.hpp
@@ -9,11 +9,11 @@ namespace klib {
     // check if we have a default stream we need to add.
     // make sure that when RTT is setup we check if we 
     // have segger support enabled.
-    #if defined(KLIB_DEFAULT_COUT) && !(KLIB_DEFAULT_COUT == rtt && KLIB_SEGGER_SUPPORT == false)
+    #if defined(KLIB_DEFAULT_COUT)
         // create a cout to print data to the host machine	
         constexpr klib::KLIB_DEFAULT_COUT::cout cout;
     #endif
-    #if defined(KLIB_DEFAULT_CIN) && !(KLIB_DEFAULT_CIN == rtt && KLIB_SEGGER_SUPPORT == false)
+    #if defined(KLIB_DEFAULT_CIN)
         // create a cin to print data to the host machine
         constexpr klib::KLIB_DEFAULT_CIN::cin cin;
     #endif
