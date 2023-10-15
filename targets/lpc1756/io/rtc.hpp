0750c2cecececf25766ea3df72c831b8e9b67110
itzandroidtab <koen@itzandroidtab.nl>
2023-10-15 02:25:02 +0200

    lpc1756 added support to set the rtc time using epoch

--- targets/lpc1756/io/rtc.hpp
+++ targets/lpc1756/io/rtc.hpp
@@ -32,6 +32,13 @@ namespace klib::lpc1756::io {
         // amount of days in a month without the leapyears
         constexpr static uint8_t month_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+        // amount of days in a year (non leap years)
+        constexpr static auto days_year = (
+            month_days[0] + month_days[1] + month_days[2] + month_days[3] +
+            month_days[4] + month_days[5] + month_days[6] + month_days[7] +
+            month_days[8] + month_days[9] + month_days[10] + month_days[11]
+        );
+
         /**
          * @brief Convert seperate date fields to the epoch time
          * 
@@ -47,13 +54,6 @@ namespace klib::lpc1756::io {
             const uint8_t hours, const uint8_t minutes, const uint8_t seconds) 
         {
             uint32_t days = 0;
-
-            // amount of days in a year (non leap years)
-            constexpr auto days_year = (
-                month_days[0] + month_days[1] + month_days[2] + month_days[3] +
-                month_days[4] + month_days[5] + month_days[6] + month_days[7] +
-                month_days[8] + month_days[9] + month_days[10] + month_days[11]
-            );
 
             // calculate the amount of days till the current year
             for (uint16_t y = 1970; y < year; y++) {
@@ -135,7 +135,40 @@ namespace klib::lpc1756::io {
          * @param time 
          */
         static void set(const klib::time::s time) {
-            // TODO: implement this
+            // get the amount of days in the epoch time
+            uint32_t days = time.value / (24 * 60 * 60);
+            uint32_t years = 1970;
+
+            // check all the years 
+            while (true) {
+                // check if the current year is a leap year
+                const uint32_t y = (
+                    (((years + 1) & 0b11) == 0) ? 
+                    days_year + 1: days_year
+                );
+
+                // check if the amount of days in the year
+                // fits in the amount of days we have left
+                if (days < y) {
+                    break;
+                }
+
+                // increment the years
+                years++;
+                days -= y;
+            }
+
+            // set the year, month and days
+            Rtc::port->YEAR = years;
+            Rtc::port->DOY = days;
+
+            // get the seconds left over
+            const uint32_t seconds = time.value % (24 * 60 * 60);
+
+            // set the hours, minutes and seconds
+            Rtc::port->HRS = seconds / (60 * 60);
+            Rtc::port->MIN = (seconds / 60) % 60;
+            Rtc::port->SEC = seconds % 60;
         }
     };
 }
