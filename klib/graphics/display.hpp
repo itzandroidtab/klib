a74fd5f02da6a160ad9c7a2706414800a5c09062
itzandroidtab <koen@itzandroidtab.nl>
2023-10-18 23:30:40 +0200

    added display orientation

---
+++ klib/graphics/display.hpp
@@ -0,0 +1,15 @@
+#ifndef KLIB_DISPLAY_HPP
+#define KLIB_DISPLAY_HPP
+
+namespace klib::graphics {
+    /**
+     * @brief Display orientations
+     * 
+     */
+    enum class orientation {
+        landscape,
+        portrait,
+    };
+};
+
+#endif
