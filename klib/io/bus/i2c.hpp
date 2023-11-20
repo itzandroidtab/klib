22e4a8e94b0b98968bf63bb5455a0a8eb002213b
itzandroidtab <koen@itzandroidtab.nl>
2023-11-21 00:49:17 +0100

    added i2c speeds

---
+++ klib/io/bus/i2c.hpp
@@ -0,0 +1,24 @@
+#ifndef KLIB_I2C_HPP
+#define KLIB_I2C_HPP
+
+#include <cstdint>
+
+namespace klib::io::i2c {
+    /**
+     * @brief Official i2c speed modes.
+     * 
+     * @details 
+     * standard = 100kbps
+     * fast = 400kbps
+     * fast plus = 1Mbps
+     * high = 3.4Mbps
+     */
+    enum class speed {
+        standard = 100'000,
+        fast = 400'000,
+        fast_plus = 1'000'000,
+        high = 3'400'000,
+    };
+}
+
+#endif
