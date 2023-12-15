61c0fc80905380538d5f2c30f2bdde801082dd3a
itzandroidtab <koen@itzandroidtab.nl>
2023-12-15 01:44:35 +0100

    added support for target lpc1759

---
+++ targets/lpc1759/io/flash.hpp
@@ -0,0 +1,12 @@
+#ifndef KLIB_LPC1759_FLASH_HPP
+#define KLIB_LPC1759_FLASH_HPP
+
+#include <cstdint>
+
+#include <targets/core/nxp/lpc17xx/flash.hpp>
+
+namespace klib::lpc1759::io {
+    using flash = core::lpc17xx::io::flash;
+}
+
+#endif
---
+++ targets/lpc1759/io/system.hpp
@@ -0,0 +1,14 @@
+#ifndef KLIB_LPC1759_SYSTEM_HPP
+#define KLIB_LPC1759_SYSTEM_HPP
+
+#include <lpc1759.hpp>
+
+#include <targets/core/nxp/lpc175x/system.hpp>
+
+namespace klib::lpc1759::io::system {
+    // import all the static classes from the core
+    // library in the current system namespace
+    using namespace core::lpc175x::io::system;
+}
+
+#endif
