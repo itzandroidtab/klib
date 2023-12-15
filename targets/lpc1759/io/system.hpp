afab51bbb91aadaa336d4b036917d95a829cf314
itzandroidtab <koen@itzandroidtab.nl>
2023-12-15 22:53:16 +0100

    updated lpc1752 and lpc1759 as well

--- targets/lpc1759/io/system.hpp
+++ targets/lpc1759/io/system.hpp
@@ -4,11 +4,16 @@
 #include <lpc1759.hpp>
 
 #include <targets/core/nxp/lpc175x/system.hpp>
+#include <targets/core/nxp/lpc17xx/system.hpp>
 
 namespace klib::lpc1759::io::system {
     // import all the static classes from the core
     // library in the current system namespace
     using namespace core::lpc175x::io::system;
+
+    // import the generic lpc17xx system drivers as 
+    // well
+    using namespace core::lpc17xx::io::system;
 }
 
 #endif
