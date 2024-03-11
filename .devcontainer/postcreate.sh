906e8a91dd0315774a7af9985222e66dbea0fc49
itzandroidtab <9889898+itzandroidtab@users.noreply.github.com>
2024-03-11 01:26:09 +0000

    devcontainer now gets all the ARM headers and generates the target headers

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -18,10 +18,27 @@ sudo rm -f /usr/bin/arm-none-eabi-objcopy || true
 sudo rm -f /usr/bin/arm-none-eabi-objdump || true
 
 echo "Linking arm-none-eabi"
-
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc 
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gdb /usr/bin/arm-none-eabi-gdb
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-size /usr/bin/arm-none-eabi-size
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-objcopy /usr/bin/arm-none-eabi-objcopy
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-objdump /usr/bin/arm-none-eabi-objdump
+
+echo "Getting arm headers"
+git clone https://github.com/ARM-software/CMSIS_5 -b develop --depth=1 CMSIS
+cp CMSIS/CMSIS/Core/Include/* klib/targets/arm/
+
+echo "Generating headers from SVD file"
+wget https://github.com/Open-CMSIS-Pack/devtools/releases/download/tools%2Fsvdconv%2F3.3.44/svdconv-3.3.44-linux64-amd64.tbz2 -O svdconv.tbz2
+tar -xf svdconv.tbz2 
+chmod +x svdconv
+
+git clone https://github.com/itzandroidtab/klib-svd
+for i in ./klib-svd/*.svd; do
+    [ -f "$i" ] || break
+
+    target=$(basename ${i%.*})
+    ./svdconv $i --suppress-warnings --generate=header -o klib/targets/$target/ || true
+    sed -i '/#include "system_/d' klib/targets/$target/$target.h
+done
