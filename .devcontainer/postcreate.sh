9e93d4f20f217fe49cea06b1df96546fd9c5c00c
itzandroidtab <9889898+itzandroidtab@users.noreply.github.com>
2024-03-11 14:55:13 +0100

    cleanup postcreate.sh

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,23 +1,19 @@
 mkdir -p ../arm-none-eabi/
 cd ../arm-none-eabi/
 
-if [ ! -f ./arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz ]
-then
-    echo "Downloading arm-none-eabi-gcc"
-    wget https://developer.arm.com/-/media/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz
-fi
+echo "Downloading arm-none-eabi-gcc"
+wget https://developer.arm.com/-/media/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz
 
 echo "Unpacking arm-none-eabi-gcc"
 tar xf arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz
 
+echo "Linking arm-none-eabi"
 sudo rm -f /usr/bin/arm-none-eabi-gcc || true
 sudo rm -f /usr/bin/arm-none-eabi-g++ || true
 sudo rm -f /usr/bin/arm-none-eabi-gdb || true
 sudo rm -f /usr/bin/arm-none-eabi-size || true
 sudo rm -f /usr/bin/arm-none-eabi-objcopy || true
 sudo rm -f /usr/bin/arm-none-eabi-objdump || true
-
-echo "Linking arm-none-eabi"
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc 
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gdb /usr/bin/arm-none-eabi-gdb
