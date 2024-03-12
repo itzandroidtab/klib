8ff87943a0fe31941f659192d5305351a168dfa3
itzandroidtab <koen@itzandroidtab.nl>
2024-03-12 06:16:47 +0100

    Added boot helper way to prevent compiliation errors when changing irq implementation to flash

--- targets/max32625/startup.cpp
+++ targets/max32625/startup.cpp
@@ -31,14 +31,21 @@ void __attribute__((__constructor__(101))) __target_startup() {
     // at runtime. When no interrupts are used this 
     // function call can be removed. By default interrupts
     // are mapped to a function that halts the whole cpu.
-    target::irq::init();
+    // this call does nothing when a flash handler is 
+    // configured
+    klib::boot_helper::init<target::irq>();
 
-    // init the systick timer
-    klib::io::systick<>::init<target::irq>();
+    // check if we can enable the systick. If we enable it
+    // here when we have a flash irq handler we will cause
+    // a hang in the default irq handler
+    if constexpr (klib::boot_helper::in_ram<target::irq>()) {
+        // init the systick timer
+        klib::io::systick<>::init<target::irq, true>();
 
-    // enable the systick timer
-    klib::io::systick<>::enable();
+        // enable the systick timer
+        klib::io::systick<>::enable();
+    }
 
-    // enable MPU, bus and usage faults in seperate interrupts
-    // SCB->SHCSR = 0b1011;
+    // enable MPU, bus and usage faults in separate inpterrupts
+    SCB->SHCSR = 0b111 << 16;
 }
