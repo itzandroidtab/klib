e5aae3771b204302db8b89f5b3b092a6e042818e
itzandroidtab <koen@itzandroidtab.nl>
2024-03-30 23:55:01 +0100

    added start of atsam4s2b

---
+++ targets/chip/atsam4s2b/startup.cpp
@@ -0,0 +1,37 @@
+#include <cstdint>
+
+#include <atsam4s2b.hpp>
+
+#include <klib/io/systick.hpp>
+
+// disable the constructor does not take arguments error in vscode
+#ifdef __INTELLISENSE__
+    #pragma diag_suppress 1094
+#endif
+
+void __attribute__((__constructor__(101))) __target_startup() {
+    namespace target = klib::atsam4s2b;
+
+    // setup the irq handler before main is called. This 
+    // moves the vector table to ram so it can be changed
+    // at runtime. When no interrupts are used this 
+    // function call can be removed. By default interrupts
+    // are mapped to a function that halts the whole cpu.
+    // this call does nothing when a flash handler is 
+    // configured
+    klib::boot_helper::init<target::irq>();
+
+    // check if we can enable the systick. If we enable it
+    // here when we have a flash irq handler we will cause
+    // a hang in the default irq handler
+    if constexpr (klib::boot_helper::in_ram<target::irq>()) {
+        // init the systick timer
+        klib::io::systick<>::init<target::irq, true>();
+
+        // enable the systick timer
+        klib::io::systick<>::enable();
+    }
+
+    // enable MPU, bus and usage faults in separate inpterrupts
+    SCB->SHCSR = 0b111 << 16;
+}
