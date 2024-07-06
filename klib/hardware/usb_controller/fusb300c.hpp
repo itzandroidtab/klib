6a7cc45f66a332557702f44c08d2f038ab0d2243
itzandroidtab <koen@itzandroidtab.nl>
2024-07-06 15:52:15 +0200

    Made a start with the fusb300c driver

---
+++ klib/hardware/usb_controller/fusb300c.hpp
@@ -0,0 +1,177 @@
+#ifndef KLIB_FUSB300C_HPP
+#define KLIB_FUSB300C_HPP
+
+#include <cstdint>
+
+#include <klib/multispan.hpp>
+
+namespace klib::hardware::usb_controller {
+    /**
+     * @brief Driver for the fusb300c
+     * 
+     * @tparam Bus 
+     * @tparam IrqPin 
+     * @tparam Address 
+     */
+    template <typename Bus, uint8_t Address = 0x22>
+    class fusb300c {
+    public:
+        // using for the callbacks
+        using interrupt_callback = void(*)();
+
+    protected:
+        // callbacks
+        static inline interrupt_callback callback = nullptr;
+
+        /**
+         * @brief All the registers available on the fusb300c
+         * 
+         */
+        enum class reg: uint8_t {
+            device_id = 0x01,
+            switches = 0x02,
+            measure = 0x04,
+            pddetvt = 0x05,
+            contorl = 0x06,
+            mask = 0x0a,
+            power = 0x0b,
+            swreset = 0x0c,
+            status0 = 0x40,
+            status1 = 0x41,
+            interrupt = 0x42,
+        };
+
+        /**
+         * @brief Write a register with data
+         * 
+         * @param command 
+         * @param data 
+         * @return status 
+         */
+        static bool write_reg(const reg command, const std::span<const uint8_t> data) {
+            // create a multispan to send
+            klib::multispan<const uint8_t> buffer = {
+                {reinterpret_cast<const uint8_t*>(&command), sizeof(command)}, 
+                data
+            };
+
+            // write the buffer to the fusb300c
+            return Bus::write(Address, buffer);
+        }  
+
+        /**
+         * @brief Write one byte to a register
+         * 
+         * @param command 
+         * @param data 
+         * @return status 
+         */
+        static bool write_reg(const reg command, const uint8_t data) {
+            // write using the other write
+            return write_reg(command, {data, sizeof(data)});
+        }  
+
+        /**
+         * @brief read multiple bytes from a register
+         * 
+         * @param command 
+         * @param data 
+         * @return status 
+         */
+        static bool read_reg(const reg command, std::span<uint8_t> data) {
+            // write the register we want to read
+            if (!Bus::write(Address, {reinterpret_cast<const uint8_t*>(&command), sizeof(command)})) {
+                return false;
+            }
+
+            // start a read
+            return Bus::read(Address, data);
+        }  
+
+        /**
+         * @brief Interrupt handler. Calls the specific function
+         * that handles the interrupt request using the irq 
+         * helper
+         * 
+         */
+        static void irq_handler() {
+            uint8_t status;
+
+            // check what interrupt we are handling
+            if (!read_reg(reg::interrupt, {&status, sizeof(status)})) {
+                return;
+            }
+
+            // call the callback
+            if (callback) {
+                callback();
+            }
+        }
+
+    public:
+        /**
+         * @brief Init the fusb300c
+         * 
+         * @tparam IrqPin 
+         * @param callback
+         * @return status 
+         */
+        static bool init() {
+            uint8_t buffer;
+
+            // check if we have a device attached
+            if (!read_reg(reg::device_id, {&buffer, sizeof(buffer)})) {
+                return false;
+            }
+            
+            // init the fusb300c
+
+            return true;
+        }
+
+        /**
+         * @brief Init and register our callback on the irq pin
+         * 
+         * @tparam IrqPin 
+         * @param callback 
+         * @return true 
+         * @return false 
+         */
+        template <typename IrqPin>
+        static bool init(const interrupt_callback callback = nullptr) {
+            // init the driver
+            if (!init()) {
+                return false;
+            }
+
+            // register our callback
+            callback = callback;
+
+            // setup and register our interrupt handler
+            IrqPin::template init<IrqPin::edge::falling>(irq_handler);
+
+            // return no errors
+            return true;
+        }
+
+        /**
+         * @brief Read if 
+         * 
+         * @return true 
+         * @return false 
+         */
+        static bool has_vbus() {
+            uint8_t buffer;
+
+            // read the status register
+            if (!read_reg(reg::status0, {&buffer, sizeof(buffer)})) {
+                return false;
+            }
+
+            // check if the vbus is high
+            return buffer & (0x1 << 7);
+        }
+    };
+}
+
+#endif
