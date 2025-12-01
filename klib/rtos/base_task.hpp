7720b4d203bad7321a65e94d2233f11fae7508fd
itzandroidtab <koen@itzandroidtab.nl>
2025-12-01 01:52:36 +0100

    Added a base task for a basic RTOS

---
+++ klib/rtos/base_task.hpp
@@ -0,0 +1,27 @@
+#ifndef KLIB_RTOS_BASE_TASK_HPP
+#define KLIB_RTOS_BASE_TASK_HPP
+
+#include <cstdint>
+
+#include <klib/units.hpp>
+
+namespace klib::rtos::detail {
+    /**
+     * @brief Task to run in the RTOS
+     * 
+     * @tparam StackSize 
+     */
+    class base_task {
+    public:
+        // time to sleep for the task
+        volatile klib::time::ms time_to_sleep = {};
+
+        // pointer to the current stack position
+        size_t* stack_pointer;
+
+        // priority of the task
+        uint8_t priority;
+    };
+}
+
+#endif
