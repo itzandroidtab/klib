5c9fcf79adf3e902699259d64db2db41b2e7e8cf
itzandroidtab <koen@itzandroidtab.nl>
2026-02-23 20:50:36 +0100

    added clocks file for stm32f469

---
+++ targets/chip/stm32f469/io/clocks.hpp
@@ -0,0 +1,10 @@
+#ifndef KLIB_STM32F469_CLOCKS_HPP
+#define KLIB_STM32F469_CLOCKS_HPP
+
+#include <targets/core/stm/stm32f4xx/clocks.hpp>
+
+namespace klib::stm32f469::io {
+    using clocks = klib::core::stm32f4xx::io::clocks;
+}
+
+#endif
