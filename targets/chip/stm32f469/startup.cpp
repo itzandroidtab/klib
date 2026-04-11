57047efb217a86a01cb7ecdc77534c63f7a7c7c8
itzandroidtab <koen@itzandroidtab.nl>
2026-04-11 12:29:39 +0200

    stm32f469 Added startup using clocks to change to 180mhz

---
+++ targets/chip/stm32f469/startup.cpp
@@ -0,0 +1,49 @@
+#include <cstdint>
+
+#include "stm32f469.hpp"
+
+#include <klib/io/systick.hpp>
+#include <klib/io/core_clock.hpp>
+#include <io/system.hpp>
+
+// disable the constructor does not take arguments error in vscode
+#ifdef __INTELLISENSE__
+    #pragma diag_suppress 1094
+#endif
+
+void __attribute__((__constructor__(101))) __target_startup() {
+    namespace target = klib::stm32f469;
+    using clock = target::io::system::clock;
+
+    // change the wait states for flash access to 5 (for 150-180Mhz)
+    target::io::system::flash::setup<5, true, true, true>();
+
+    // setup the clock to 180Mhz (in this example we are using an 8Mhz
+    // oscillator)
+    // ((8Mhz * 360) / 8 = 360Mhz) / 2 = 180Mhz
+    target::io::system::clock::set_main<target::io::system::crystal::source::external, 8'000'000, 360, 8, 2>();
+    // target::io::system::clock::set_main<target::io::system::crystal::source::internal, 16'000'000, 360, 16, 2>();
+
+    // setup the irq handler before main is called. This
+    // moves the vector table to ram so it can be changed
+    // at runtime. When no interrupts are used this
+    // function call can be removed. By default interrupts
+    // are mapped to a function that halts the whole cpu.
+    // this call does nothing when a flash handler is
+    // configured
+    klib::irq::boot_helper::init<target::irq>();
+
+    // check if we can enable the systick. If we enable it
+    // here when we have a flash irq handler we will cause
+    // a hang in the default irq handler
+    if constexpr (klib::irq::boot_helper::in_ram<target::irq>()) {
+        // init the systick timer
+        klib::io::systick<>::init<target::irq, true>();
+
+        // enable the systick timer
+        klib::io::systick<>::enable();
+    }
+
+    // enable MPU, bus and usage faults in separate inpterrupts
+    SCB->SHCSR = 0b111 << 16;
+}
